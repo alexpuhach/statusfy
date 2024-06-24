f7aba8b6baa1fc9afc8dbe91e6415128a27517a9
Alex Puhach <spugac57@gmail.com>
2024-06-24 18:44:41 +0200

    Initialize gh-pages branch

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "title": "test",
+  "name": "test",
+  "description": "test",
+  "defaultLocale": "en",
+  "locales": [
+    {
+      "code": "en",
+      "iso": "en-US",
+      "name": "English",
+      "file": "en.json"
+    }
+  ],
+  "content": {
+    "frontMatterFormat": "yaml"
+  }
+}
